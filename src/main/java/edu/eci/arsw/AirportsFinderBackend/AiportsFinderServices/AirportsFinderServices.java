4ad59b86e3f42edc504541f2b2c927c4dec5f4f0
GYEISSON <yeison7f@gmail.com>
2020-03-18 00:47:58 -0500

    configurando1

--- src/main/java/edu/eci/arsw/AirportsFinderBackend/AiportsFinderServices/AirportsFinderServices.java
+++ src/main/java/edu/eci/arsw/AirportsFinderBackend/AiportsFinderServices/AirportsFinderServices.java
@@ -1,12 +1,19 @@
 
-package edu.eci.arsw.AirportsFinderBackend.AirportsFinderServices;
+package edu.eci.arsw.AirportsFinderBackend.AiportsFinderServices;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /*
  *
  * @author yeisson Gualdron
  */
-@Service
-public class AirportsFinderServices {
+
+public interface AirportsFinderServices {
 
 }
--- src/main/java/edu/eci/arsw/AirportsFinderBackend/AiportsFinderServices/HttpConnectionService.java
+++ src/main/java/edu/eci/arsw/AirportsFinderBackend/AiportsFinderServices/HttpConnectionService.java
@@ -1,6 +1,13 @@
 
-package edu.eci.arsw.AirportsFinderBackend.AirportsFinderServices;
+package edu.eci.arsw.AirportsFinderBackend.AiportsFinderServices;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /*
  *
