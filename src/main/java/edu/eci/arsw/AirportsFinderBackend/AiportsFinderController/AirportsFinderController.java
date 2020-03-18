4e14199caaf39596e37c6740128188b20d0a2796
GYEISSON <yeison7f@gmail.com>
2020-03-18 00:39:09 -0500

    configurando

--- src/main/java/edu/eci/arsw/AirportsFinderBackend/AiportsFinderController/AirportsFinderController.java
+++ src/main/java/edu/eci/arsw/AirportsFinderBackend/AiportsFinderController/AirportsFinderController.java
@@ -1,6 +1,15 @@
 
-package edu.eci.arsw.AirportsFinderBackend.AirportsFinderController;
-
+package edu.eci.arsw.AirportsFinderBackend.AiportsFinderController;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
 *
